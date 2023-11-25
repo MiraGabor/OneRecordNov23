3027d139d329d165827679bd919bf10134e22e55
lrou <leo.roubertoux@swiss.com>
2023-11-25 07:54:48 +0100

    Forgotten in the commit

--- src/OneCheck.Repository/Model/DestinationConsigneeSheetModel.cs
+++ src/OneCheck.Repository/Model/DestinationConsigneeSheetModel.cs
@@ -5,6 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace OneCheck.Repository.Model;
-internal class DestinationConsigneeSheetModel
+public class DestinationConsigneeSheetModel : CheckSheetModel
 {
+    public bool IsContainerDamaged { get; set; }
+    public bool IsContainerOperating { get; set; }
+    public int DisplayTemp { get; set; }
+    public bool Is5AdaptersPresents { get; set; }
+    public string RecipientName { get; set; }
+    public string RecipientSignature { get; set; }
 }
--- src/OneCheck.Repository/Model/OriginPreparationSheetModel.cs
+++ src/OneCheck.Repository/Model/OriginPreparationSheetModel.cs
@@ -13,7 +13,7 @@ public class OriginPreparationSheetModel : CheckSheetModel
     public bool IsContainerDamaged { get; set; }
     public List<string> Routing { get; set; }
     public int GoodsPrecooledAtTemps { get; set; }
-    public int PrecooledTemp { get; set; }
+    public int ULDPrecooledTemp { get; set; }
     public int TempSetting { get; set; }
     public int AcceptedTempRange { get; set; }
     public string EmergencyPhone {  get; set; }
