1b6af008585e4d1026a6c089106b6bc9e1ecf34d
lrou <leo.roubertoux@swiss.com>
2023-11-25 07:46:45 +0100

    AddModel

---
+++ src/OneCheck.Repository/Model/OriginPreparationSheetModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneCheck.Repository.Model;
+public class OriginPreparationSheetModel : CheckSheetModel
+{
+    public string ULDNumber {  get; set; }
+    public int AWBNumber { get; set; }
+    public bool IsBatteryCharged { get; set; }
+    public bool IsContainerDamaged { get; set; }
+    public List<string> Routing { get; set; }
+    public int GoodsPrecooledAtTemps { get; set; }
+    public int PrecooledTemp { get; set; }
+    public int TempSetting { get; set; }
+    public int AcceptedTempRange { get; set; }
+    public string EmergencyPhone {  get; set; }
+}
