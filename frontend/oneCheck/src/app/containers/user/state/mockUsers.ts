db452e00d7635d233235fb0a0b000fa958cb7966
Nikolai Weidner <niko.weidner@gmx.de>
2023-11-25 22:57:30 +0300

    [Frontend] Add todo and logos

--- frontend/oneCheck/src/app/containers/user/state/mockUsers.ts
+++ frontend/oneCheck/src/app/containers/user/state/mockUsers.ts
@@ -1,34 +1,41 @@
 import { User } from '../models/user.model';
 
+// todo role
 export const mockUsers: User[] = [
   {
     id: '0',
-    name: 'Leonardo R.',
+    name: 'Nikolai Weidner',
     stationName: 'ZRH',
+    role: 'export',
   },
   {
     id: '1',
     name: 'Robbert Bobbert',
     stationName: 'JFK',
+    role: 'export',
   },
   {
     id: '2',
     name: 'Dj Shashank',
     stationName: 'JFK',
+    role: 'export',
   },
   {
     id: '3',
     name: 'Miroslav Gabbbbor',
     stationName: 'DHL', // todo
+    role: 'export',
   },
   {
     id: '4',
     name: 'D. Ante',
     stationName: 'DHL', // todo
+    role: 'export',
   },
   {
     id: '5',
     name: 'Nikolaiowitch',
     stationName: 'DHL', // todo
+    role: 'export',
   },
 ];
