76cdf894667354c395a414dd261a8ad4d4312d13
lrou <leo.roubertoux@swiss.com>
2023-11-25 09:52:26 +0100

    Add empty object

--- src/OneCheck.Application/Dtos/SheetsDtos/SheetDto.cs
+++ src/OneCheck.Application/Dtos/SheetsDtos/SheetDto.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OneCheck.Application.Dtos.SheetsDtos;
 public class SheetDto
 {
+    public SheetDto() 
+    {
+        this.PreparationSheet =  new OriginPreparationSheetDto();
+        this.InboundTransitSheets  = new List<InboundTransitSheetDto>();
+        this.OriginHandlingAgentSheet = new OriginHandlingAgentSheetDto();
+        this.DestinationHandlingAgentSheet = new DestinationHandlingAgentSheetDto();
+        this.DestinationConsigneeSheet = new DestinationConsigneeSheetDto();
+    }
     public Guid CheckSheetId { get; set; }
 
     public OriginPreparationSheetDto PreparationSheet { get; set;}
